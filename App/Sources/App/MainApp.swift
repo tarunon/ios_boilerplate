4a86dbb95037de50a9c9de6e54de96571993c9fd
tarunon <croissant9603@gmail.com>
2021-09-27 00:50:34 +0900

    modify: use SwiftUI.App

---
+++ App/Sources/App/MainApp.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  App
+//
+//  Created by tarunon on 2021/05/15.
+//
+
+import Component
+import SwiftUI
+
+@main @MainActor
+struct MainApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
